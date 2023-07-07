36cce468a950fbe1be74887b6f5cc6d1d3b2513d
Sosuke1019 <kato.052wazime@gmail.com>
2023-07-07 23:42:27 +0900

    Fix interp.rb 演算と比較式の追加

--- interp.rb
+++ interp.rb
@@ -17,11 +17,30 @@ def evaluate(tree)
         left = evaluate(tree[1])
         right = evaluate(tree[2])
         left * right
-    else
-        # ここではtree[0] == "/"を意味する
+    when "%"
         left = evaluate(tree[1])
         right = evaluate(tree[2])
-        left / right
+        left % right
+    when "<"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left < right
+    when "<="
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left <= right
+    when "=="
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left == right
+    when ">="
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left >= right
+    when ">"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left > right
     end
 end
 
