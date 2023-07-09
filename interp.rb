b2c64d01881e65ed470befbe924ba4618001c6d1
Sosuke1019 <kato.052wazime@gmail.com>
2023-07-09 17:43:35 +0900

    feat: while分に対応するために、evaluate関数の分岐を追加

--- interp.rb
+++ interp.rb
@@ -4,11 +4,16 @@ require "minruby"
 # 演算結果を返す関数
 def evaluate(tree, env)
     case tree[0]
-    when "if"
+    when 'if'
         if evaluate(tree[1], env)
             evaluate(tree[2], env)
         else
             evaluate(tree[3], env)
+        end
+    when "while"
+        while evaluate(tree[1], env)
+            evaluate(tree[2], env)
+        end
     when "lit"
         tree[1]
     when "+"
