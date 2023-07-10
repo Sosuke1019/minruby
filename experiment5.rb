65d09e8486ce3f46d3d353b2e0afd6a15ecadfdf
Sosuke1019 <kato.052wazime@gmail.com>
2023-07-10 14:53:09 +0900

    fix: 組み込み関数に対応するために、関数名のための環境を用意

--- experiment5.rb
+++ experiment5.rb
@@ -1,15 +1,20 @@
 require "minruby"
 pp(minruby_parse("
-answer = 0
 i = 1
-hhhh = 0
-while i <= 1000
-    if i % 2 == 0
-        answer = answer + i
+while i <= 100
+    if i % 3 == 0
+        if i % 5 == 0
+            p("FizzBuzz")
+        &else
+            p("Fizz")
+        end 
     else
-        hhhh = hhhh - 1
+    if i % 5 == 0
+        p("Buzz")
+    else
+        p(i)
     end
+end
     i = i + 1
 end
-p(answer)
 "))
--- interp.rb
+++ interp.rb
@@ -2,88 +2,88 @@ require "minruby"
 
 
 # 演算結果を返す関数
-def evaluate(tree, env)
+def evaluate(tree, venv, lenv)
     case tree[0]
     when "lit"
         tree[1]
     when "+"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left + right
     when "-"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left - right
     when "*"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left * right
     when '/'
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left / right
     when "%"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left % right
     when '**'
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left**right
     when '=='
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left == right
     when '!='
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left != right
     when "<"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left < right
     when "<="
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left <= right
     when ">="
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left >= right
     when ">"
-        left = evaluate(tree[1], env)
-        right = evaluate(tree[2], env)
+        left = evaluate(tree[1], genv, lenv)
+        right = evaluate(tree[2], genv, lenv)
         left > right
     when "func_call" # 関数の対応
-        p evaluate(tree[2], env) 
+        p evaluate(tree[2], genv, lenv) 
     when 'stmts' # 複文の対応
         i = 1
         last = nil
         while tree[i]
-            last = evaluate(tree[i], env)
+            last = evaluate(tree[i], genv, lenv)
             i = i + 1
         end
     last
     when 'var_assign' # 変数を代入する
-        env[tree[1]] = evaluate(tree[2], env)
+        lenv[tree[1]] = evaluate(tree[2], genv, lenv)
     when 'var_ref' # 変数を参照する
-        env[tree[1]]
+        lenv[tree[1]]
     when "if"
-        if evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        if evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         else
             # else節が存在する場合にのみ実行する処理
             # 右辺の条件がfalseの場合に左辺の式を評価する「後置unless(unless修飾子)」
-            evaluate(tree[3], env) unless tree[3].nil?
+            evaluate(tree[3], genv, lenv) unless tree[3].nil?
         end
     when "while"
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        while evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         end
     when "while2"
-        evaluate(tree[2], env)
-        while evaluate(tree[1],env)
-            evaluate(tree[2], env)
+        evaluate(tree[2], genv, lenv)
+        while evaluate(tree[1],venv, lenv)
+            evaluate(tree[2], genv, lenv)
         end
     else
         p("error")
@@ -132,6 +132,7 @@ tree = minruby_parse(str)
 
 # ③ 計算の木を実行（計算）する
 # 環境の初期状態
-env = {}
+genv = { "p" => ["builtin", "p"]}
+lenv = {}
 # 抽象構文木と環境を指定して実行開始
-evaluate(tree, env)
+evaluate(tree, venv, lenv)
